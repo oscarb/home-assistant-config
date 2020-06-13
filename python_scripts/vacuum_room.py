8c239c8e5e699af7577fe1be229172145cb9c21b
Oscar <git@oscarb.se>
2020-06-13 20:02:36 +0200

    Add python script

---
+++ python_scripts/vacuum_room.py
@@ -0,0 +1,33 @@
+# Vacuum a specific room
+
+rooms = []
+roomString = data.get("room").lower()
+
+if "kitchen" in roomString:
+    rooms.append(19)
+if "living room" in roomString:
+    rooms.append(29)
+if "kitchen" in roomString:
+    rooms.append(19)
+if "kitchen" in roomString:
+    rooms.append(19)
+if "kitchen" in roomString:
+    rooms.append(19)
+if "kitchen" in roomString:
+    rooms.append(19)
+
+
+rooms = []
+roomString = data.get("room")
+
+room = data.get("room")
+logger.info("Hello %s", name)
+hass.bus.fire(name, {"wow": "from a Python script!"})
+
+
+# turn_on_light.py
+entity_id = data.get("entity_id")
+
+if entity_id is not None:
+    service_data = {"entity_id": entity_id, "command": "app_segment_clean", "params": [19]}
+    hass.services.call("vacuum", "send_command", service_data, False)
