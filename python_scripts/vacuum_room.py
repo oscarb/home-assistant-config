3f9c6cb662cf1f74afd865a29703d2cb13cc983a
oscarb <git@oscarb.se>
2020-10-18 15:35:34 +0200

    Add selective room cleaning

--- python_scripts/vacuum_room.py
+++ python_scripts/vacuum_room.py
@@ -1,28 +1,26 @@
 # Vacuum a specific room
 
-rooms = []
-roomString = data.get("room").lower()
+roomConfig = {
+    16: ["hallway"],
+    17: ["bedroom"],
+    18: ["entrance"],
+    19: ["kitchen"],
+    20: ["nursery", "kids room"],
+    22: ["living room"],
+    23: ["office"] 
+}
 
-if "kitchen" in roomString:
-    rooms.append(19)
-if "living room" in roomString:
-    rooms.append(29)
-if "kitchen" in roomString:
-    rooms.append(19)
-if any(room in roomString for room in ["nursery", "kids room"]]):
-    rooms.append(19)
-
-rooms = []
-roomString = data.get("room")
-
-room = data.get("room")
-logger.info("Hello %s", name)
-hass.bus.fire(name, {"wow": "from a Python script!"})
-
-
-# turn_on_light.py
 entity_id = data.get("entity_id")
+area = data.get("area").lower()
 
-if entity_id is not None:
-    service_data = {"entity_id": entity_id, "command": "app_segment_clean", "params": [19]}
+roomsToClean = []
+
+for roomNumber, roomNames in roomConfig.items():
+    for name in roomNames:
+        if name in area: 
+            roomsToClean.append(int(roomNumber))
+            continue
+
+if entity_id is not None and len(roomsToClean) > 0: 
+    service_data = {"entity_id": entity_id, "command": "app_segment_clean", "params": roomsToClean}
     hass.services.call("vacuum", "send_command", service_data, False)
